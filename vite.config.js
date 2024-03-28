d2bd765122b89e8ee325e78ca003ec493614de6d
Mostafa Personal <mostafa.kader2350@gmail.com>
2024-03-28 15:38:55 +0200

    asd

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
 
-  plugins: [react()],
+  if (command !== "serve") {
+    config.base = "/react-vite-gh-pages/";
+  }
+
+  return config;
 });
